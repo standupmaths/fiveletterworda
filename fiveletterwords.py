3dc66ed2df06a382ed9115cbc7565a18ac43c8e9
Ricardo Fernández Serrata <76864299+Rudxain@users.noreply.github.com>
2022-10-19 20:49:07 -0400

    Update fiveletterwords.py

--- fiveletterwords.py
+++ fiveletterwords.py
@@ -5,23 +5,26 @@ start_time = time.time()
 
 filestub = '/Users/mattparker/Dropbox/python/five_letter_words/'
 
+
 def load_words():
     words_txt = f'{filestub}words_alpha.txt'
-    with open(words_txt) as word_file:
+    with open(words_txt, encoding='UTF-8') as word_file:
         valid_words = word_file.read().split()
     return valid_words
 
-word_length = 5
 
-word_length2 = word_length*2
-word_length4 = word_length2*2
+word_length: Final = 5
+
+word_length2 = word_length * 2
+word_length4 = word_length2 * 2
 word_length5 = word_length4 + word_length
 
 # number of scanA increases per progress report
 stepgap = 1000
 
 # Yes, that is the alphabet. In the default order python makes a list in. Weird.
-alphabet = ['f', 'g', 'o', 'q', 't', 'b', 'y', 'h', 'r', 'u', 'j', 'w', 'i', 'p', 's', 'd', 'l', 'e', 'k', 'm', 'n', 'v', 'z', 'c', 'a', 'x']
+alphabet = ['f', 'g', 'o', 'q', 't', 'b', 'y', 'h', 'r', 'u', 'j', 'w',
+            'i', 'p', 's', 'd', 'l', 'e', 'k', 'm', 'n', 'v', 'z', 'c', 'a', 'x']
 
 # I could be clever and write this to be dynamic
 # but for now I'll hard code everything assuming five words
@@ -36,9 +39,9 @@ fl_words = [w for w in english_words if len(w) == word_length]
 print(f"{len(fl_words)} words have {word_length} letters")
 
 
-word_sets = []
+word_sets: list[set[str]] = []
+unique_fl_words: list[str] = []
 
-unique_fl_words = []
 for w in fl_words:
     unique_letters = set(w)
     if len(unique_letters) == word_length:
@@ -50,8 +53,8 @@ number_of_words = len(unique_fl_words)
 
 print(f"{number_of_words} words have a unique set of {word_length} letters")
 
-doubleword_sets = []
-doubleword_words = []
+doubleword_sets: list[set[str]] = []
+doubleword_words: list[str] = []
 
 scanA = 0
 while scanA + 1 < number_of_words:
@@ -60,7 +63,8 @@ while scanA + 1 < number_of_words:
         give_it_a_try = word_sets[scanA] | word_sets[scanB]
         if len(give_it_a_try) == word_length2:
             doubleword_sets.append(give_it_a_try)
-            doubleword_words.append([unique_fl_words[scanA], unique_fl_words[scanB]])
+            doubleword_words.append(
+                [unique_fl_words[scanA], unique_fl_words[scanB]])
         scanB += 1
     scanA += 1
 
@@ -75,7 +79,7 @@ success_found = []
 scanA = 0
 print(f"starting at position {scanA}")
 
-while scanA < number_of_doublewords-1:
+while scanA < number_of_doublewords - 1:
     if scanA % stepgap == 0:
         print(f"Up to {scanA} after {time.time() - start_time} seconds.")
 
